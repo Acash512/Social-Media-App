0233dc8f260ab2529bbf1147df28438ed87c69fd
Acash512 <aakashguptaa23@gmail.com>
2021-10-16 23:35:25 +0530

    Implemented controllers for creating and querying posts

---
+++ src/controllers/posts.js
@@ -0,0 +1,27 @@
+const {Posts,Users} = require('../db/models')
+
+async function createNewPost(title,body,userId){
+    const post = await Posts.create({
+        title,
+        body,
+        userId
+    })
+
+    return post
+}
+
+async function showAllPosts(){
+    const posts = await Posts.findAll({
+        include:[Users]
+    })
+
+    return posts
+}
+
+async function showAllPostsForUser(userId){
+    const posts = await Posts.findAll({
+        where:{userId}
+    })
+
+    return posts 
+}
